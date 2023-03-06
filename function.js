0e7a0247b92055bd2d1f67f31bbdca9d36658e05
ArturMagri <arturo.magri@gmail.com>
2023-03-06 20:04:38 -0300

    corrigiendo formulario

--- function.js
+++ function.js
@@ -1,3 +1,19 @@
-document.getElementById('boton').addEventListener('click', function(){
+console.log("Hola mundo!");
+
+var input = document.querySelector("input");
+
+input.addEventListener("change", (e) =>{
+        console.log(e);
+        const text = input.value;
+        console.log(input.value);
+        e.preventDefault();
+});
+
+document.getElementById('boton').addEventListener('click', clickBoton);
+
+function clickBoton() {
+        console.log("El usuario hizo click en el boton");
         document.getElementById('enviado').style.display = 'block';
-})
+        console.log(input.value)
+        alert("Mensaje enviado!" + " " +input.value)
+}
